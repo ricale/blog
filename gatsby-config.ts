ce96e1e497a5c1dae9ddc69a084eebd182506314
KIM Kangseong <KIM.Kangseong@gmail.com>
2023-01-07 10:21:40 +0900

    튜토리얼 보며 프로젝트 초기화

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,20 +3,34 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `ricalelog`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blog",
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+  ],
 };
 
 export default config;
---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Layout from '../components/Layout';
+import Seo from '../components/Seo';
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle='Home Page'>
+      <p>this is about</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>about</title>
+
+export default AboutPage
