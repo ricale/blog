eefc0e59c8ad9b6290ac64ca67138ee001fdcbd1
KIM Kangseong <KIM.Kangseong@gmail.com>
2023-01-12 20:24:29 +0900

    배포 설정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `ricalelog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  pathPrefix: "/blog",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
