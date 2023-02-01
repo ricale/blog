21dd46f562b4e35dc24e1c247df9a6a48427b7ca
KIM Kangseong <KIM.Kangseong@gmail.com>
2023-02-01 19:38:27 +0900

    Utterances 대신 Giscus 적용

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -5,13 +5,30 @@ function Comments() {
 
   useEffect(() => {
     const scriptTag = document.createElement("script");
+    // NOTE: Utterances 를 위한 설정 코드.
+    // const attrs = {
+    //   src: "https://utteranc.es/client.js",
+    //   repo: "ricale/blog",
+    //   "issue-term": "pathname",
+    //   label: "utterances",
+    //   theme: "github-dark",
+    //   crossOrigin: "anonymous",
+    //   async: "true",
+    // };
     const attrs = {
-      src: "https://utteranc.es/client.js",
-      repo: "ricale/blog",
-      "issue-term": "pathname",
-      label: "utterances",
-      theme: "github-dark",
-      crossOrigin: "anonymous",
+      src: "https://giscus.app/client.js",
+      "data-repo": "ricale/blog",
+      "data-repo-id": "R_kgDOIvqo5g",
+      "data-category": "Giscus",
+      "data-category-id": "DIC_kwDOIvqo5s4CT7fR",
+      "data-mapping": "pathname",
+      "data-strict": "0",
+      "data-reactions-enabled": "1",
+      "data-emit-metadata": "0",
+      "data-input-position": "top",
+      "data-theme": "dark",
+      "data-lang": "ko",
+      crossorigin: "anonymous",
       async: "true",
     };
     Object.entries(attrs).forEach(([key, value]) => {
